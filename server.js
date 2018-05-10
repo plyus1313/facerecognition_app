9c59de8c1e058cda862cbce573ffe50cc95ac5d7
Viktoriya Savaryn <vsviktoriya13@gmail.com>
2018-05-10 15:14:41 +0200

    abdation db

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-flexible-35603',
     user : 'postgres',
     password : 'motorola1111',
     database : 'smart'
